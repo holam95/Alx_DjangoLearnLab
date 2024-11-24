b2432e58867234f38d5ef1a863e5093461155cd0
holam95 <holambis@outlook.com>
2024-11-24 11:57:14 -0800

    added query_samples.py,models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    author = models.CharField()
+
+class Book(models.Model):
+    title = CharField()
+    author = ForeignKey(Author, on_delete = models.CASCADE, name = 'books')
+
+class Library(models.Model):
+    name = CharField()
+    books = ManyToManyField(Book, name = 'library')
+
+class Librarian(models.Model):
+    name = CharField()
+    library = OneToOneField(Library , on_delete = models.CASCADE, name = 'librarian')

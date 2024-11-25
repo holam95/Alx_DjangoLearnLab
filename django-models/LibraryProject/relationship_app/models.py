a3702a115fc21095c5eadc56e4144cb0ecd78678
holam95 <holambis@outlook.com>
2024-11-25 07:12:16 -0800

    changes models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,15 +4,24 @@ from django.db import models
 
 class Author(models.Model):
     author = models.CharField()
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     title = CharField()
     author = ForeignKey(Author, on_delete = models.CASCADE, name = 'books')
+    def __str__(self):
+        return self.title
 
 class Library(models.Model):
     name = CharField()
     books = ManyToManyField(Book, name = 'library')
+    def __str__(self):
+        return self.name
 
 class Librarian(models.Model):
     name = CharField()
     library = OneToOneField(Library , on_delete = models.CASCADE, name = 'librarian')
+
+    def __str__(self):
+        return self.name

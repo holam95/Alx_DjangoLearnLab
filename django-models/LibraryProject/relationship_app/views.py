b6aff818e7fab7bf768906ad5793013c24887f06
holam95 <holambis@outlook.com>
2024-11-28 10:50:13 -0800

    changes,views.py,urls.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
-
+from .models import Book,Author
 # Create your views here.
+def book_list(request):
+    books = Book.objects.all
+    context = {books:author)
+
+    return render(request, "books/book_list.html", context)
+
+from django.views.generic import ListView
+
+class BookListiListView(ListView):
+    model = Book
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

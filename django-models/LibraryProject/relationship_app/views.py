cd3cc4dd2f911431ec981fbe25bf4c193dcf8be2
holam95 <holambis@outlook.com>
2024-11-29 08:28:27 -0800

    ch templates added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Book,Author
+from relationship_app.templates import library_detail.html
+from relationship_app.templates import list_books.html
 # Create your views here.
 def list_books(request):
     books = Book.objects.all

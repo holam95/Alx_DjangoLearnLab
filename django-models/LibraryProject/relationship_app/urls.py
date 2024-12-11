1984239b03f25a4b9f3b88f8769da94c4f156c6d
Holam95 <holambis@gmail.com>
2024-12-11 01:44:00 +0100

    changes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import LibraryDetailView
+from .views import LibraryDetailView, CustomLoginView, CustomLogoutView, RegisterView
 
 from .views import list_books
 
 urlpatterns = [
         path('Library/', LibraryDetailView.as_view()),
 
-        path('Book/', list_book.as_view()]
+        path('Book/', list_book.as_view(
+        path('login/', CustomLoginView.as_view(), name='login'),
+        path('logout/', CustomLogoutView.as_view(), name='logout'),
+        path('register/', RegisterView.as_view(), name='register')]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from .models import Book,Author
 from relationship_app.templates import library_detail.html
 from relationship_app.templates import list_books.html
+
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth import login
+
 # Create your views here.
 def list_books(request):
     books = Book.objects.all
@@ -23,6 +30,26 @@ class LibraryDetailView(DetailView):
 
 
 
+
+# Login View
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+    redirect_authenticated_user = True  # Redirect if the user is already authenticated
+
+# Logout View
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+# Registration View
+class RegisterView(FormView):
+    template_name = 'relationship_app/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')  # Redirect to login page after successful registration
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)  # Automatically log in the user after registration
+        return super().form_valid(form)
 
     
 

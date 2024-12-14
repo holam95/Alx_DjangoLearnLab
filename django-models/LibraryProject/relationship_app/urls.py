a7b7e02e1f3d848fe8fe6ab04485663f292eef18
Holam95 <holambis@gmail.com>
2024-12-14 19:02:56 +0100

    correction to urls,views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,27 +1,15 @@
 from django.urls import path
-from .views import LibraryDetailView, LoginView, LogoutView, register
+from .views import LibraryDetailView, LoginView, LogoutView, register, list_books
 from .admin_view import admin_view
 from .librarian_view import librarian_view
 from .member_view import member_view
 
-from .views import list_books
-
-
-
-
-
-
-
-
-
-
-urlpatterns = [path('Library/', LibraryDetailView.as_view()),]
-[path('Book/', list_book.as_view, name='list_books')]
-[path('login/', LoginView.as_view(template_name=login.html), name='login'),]
-[path('logout/', LogoutView.as_view(template_name=logout.html ), name='logout'),]
-[path('register/', views.register(template_name=register.html), name='register')]
-[path('admin/', admin_view, name='admin_view'),]
-[path('librarian/', librarian_view, name='librarian_view'),]
-[path('member/', member_view, name='member_view'),]
-        
-        
+urlpatterns = [
+    path('library/', LibraryDetailView.as_view(), name='library_detail'),  # Corrected to lowercase URL
+    path('books/', list_books, name='list_books'),  # Corrected to lowercase URL
+    path('login/', LoginView.as_view(), name='login'),  # Template set in views.py
+    path('logout/', LogoutView.as_view(), name='logout'),  # Template set in views.py
+    path('register/', register, name='register'),
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,67 +1,52 @@
-from django.shortcuts import render
-from .models import Book,Author
-from relationship_app.templates import library_detail.html
-from relationship_app.templates import list_books.html
-
 from django.shortcuts import render, redirect
-from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic.edit import FormView
+from .models import Book, Author, Library
+from django.contrib.auth.views import LoginView as BaseLoginView, LogoutView as BaseLogoutView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.contrib.auth import login
-
-
 from .utils import role_required
-
 
 from .admin_view import admin_view
 from .librarian_view import librarian_view
 from .member_view import member_view
 
-
+# Role-Based Views
 @role_required('Admin')
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html', {})
-
 
 @role_required('Librarian')
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html', {})
 
-
 @role_required('Member')
 def member_view(request):
     return render(request, 'relationship_app/member_view.html', {})
 
-# Create your views here.
+# Function-Based View for Listing Books
 def list_books(request):
-    books = Book.objects.all
-    context = {books:author)
+    books = Book.objects.all()  # Added parentheses to call the queryset
+    context = {'books': books}  # Fixed the dictionary key-value pair syntax
+    return render(request, "relationship_app/list_books.html", context)
 
-    return render(request, "relationship_app/list_books.html", Book.objects.all())
-
-
-from django.views.generic.detail import DetailView
-from . models import Library
+# Class-Based View for Library Details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relatioship_app/library_datail.html'
+    template_name = 'relationship_app/library_detail.html'  # Corrected template name
 
-    def AllBooks(self):
-
-        Allbooks = library.books.all()
-        return render("relationship_app/library_detail.html", from .models import Library)
-
-
-
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()  # Fetch all books related to the library
+        return context
 
 # Login View
-class LoginView(LoginView):
+class LoginView(BaseLoginView):
     template_name = 'relationship_app/login.html'
     redirect_authenticated_user = True  # Redirect if the user is already authenticated
 
 # Logout View
-class LogoutView(LogoutView):
+class LogoutView(BaseLogoutView):
     template_name = 'relationship_app/logout.html'
 
 # Registration View
@@ -75,8 +60,6 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
-
-    
 
 
 
